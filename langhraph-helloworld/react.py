23405872bb7bc69fb452b85c4eb042123d485ebb
Ganes769 <ganeshsnawali@gmail.com>
2026-09-23 18:12:42 +0100

    temp triple

--- langhraph-helloworld/react.py
+++ langhraph-helloworld/react.py
@@ -1,1 +1,15 @@
 # Reasoning engine 
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_groq import ChatGroq
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+@tool
+def triple (num:float)-> float:
+    """param num : a number to triple
+    return : a triple of a param number
+    """
+    return float(num)*3
+tools=[TavilySearch(max_results=1), triple]
+llm=ChatGroq(   model="openai/gpt-oss-20b",temperature=0).bind_tools(tools=tools)
